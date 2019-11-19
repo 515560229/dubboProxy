ada1875eed82aedbc4ff5a83c5c25a6da4651a5d
mayonglin <mayonglin@sf-express.com>
2019-11-19 13:44:35 +0800

    新增dubboRpcProxyV2，将dubbo的通用数据放header。

--- src/main/java/vip/javacoder/dubboproxy/controller/DubboProxyController.java
+++ src/main/java/vip/javacoder/dubboproxy/controller/DubboProxyController.java
@@ -4,9 +4,13 @@ package vip.javacoder.dubboproxy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import vip.javacoder.dubboproxy.dubbo.DubboServiceFactory;
 import vip.javacoder.dubboproxy.model.Input;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class DubboProxyController {
@@ -15,7 +19,29 @@ public class DubboProxyController {
     private DubboServiceFactory factory;
 
     @PostMapping({"/dubboRpcProxy"})
-    Object dubboRpcProxy(@RequestBody Input input) {
+    public Object dubboRpcProxy(@RequestBody Input input) {
+        return this.factory.genericInvoke(input);
+    }
+
+    @PostMapping({"/dubboRpcProxyV2"})
+    public Object dubboRpcProxy(
+            @RequestHeader(value = "dubbo-url") String url,
+            @RequestHeader(value = "dubbo-group", required = false) String group,
+            @RequestHeader(value = "dubbo-interfaceName") String interfaceName,
+            @RequestHeader(value = "dubbo-methodName") String methodName,
+            @RequestHeader(value = "dubbo-parameterTypes") String parameterTypes,
+            @RequestHeader(value = "dubbo-version", required = false) String version,
+            @RequestHeader(value = "dubbo-timeout", required = false) Integer timeout,
+            @RequestBody List<Object> parameters) {
+        Input input = new Input();
+        input.setInterfaceName(interfaceName);
+        input.setMethodName(methodName);
+        input.setParameterTypes(Arrays.asList(parameterTypes.split(",")));
+        input.setParameters(parameters);
+        input.setUrl(url);
+        input.setVersion(version);
+        input.setTimeout(timeout);
+        input.setGroup(group);
         return this.factory.genericInvoke(input);
     }
 }
